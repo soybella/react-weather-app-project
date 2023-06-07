713f5ddc32e8a57cc8973ceabe29782706730b89
Isabella <isabellaj.lizarde@gmail.com>
2023-06-06 21:08:43 -0700

    Created new component to track current temperature unit

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,19 +2,29 @@ import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
   const [unit, setUnit] = useState("fahrenheit");
-  function convertToCelsius(event) {
+  function showCelsius(event) {
     event.preventDefault();
     setUnit("celsius");
   }
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function celsius() {
+    return ((props.fahrenheit - 32) * 5) / 9;
+  }
+
   if (unit === "fahrenheit") {
     return (
       <span className="WeatherTemperature">
         <span className="current-temp" id="current-temp">
           {Math.round(props.fahrenheit)}
-        </span>
-        <span className="fahrenheit-symbol active" id="fahrenheit-symbol">
+        </span>{" "}
+        <span className="temperature-symbol active" id="fahrenheit-symbol">
           °F |
-          <a href="/" className="celsius-symbol" onClick={convertToCelsius}>
+          <a href="/" className="temperature-symbol" onClick={showCelsius}>
             {" "}
             °C{" "}
           </a>
@@ -22,6 +32,18 @@ export default function WeatherTemperature(props) {
       </span>
     );
   } else {
-    return "C";
+    return (
+      <span className="WeatherTemperature">
+        <span className="current-temp" id="current-temp">
+          {Math.round(celsius())}
+        </span>{" "}
+        <span className="temperature-symbol" id="fahrenheit-symbol">
+          <a href="/" className="temperature-symbol" onClick={showFahrenheit}>
+            °F{" "}
+          </a>{" "}
+          | °C
+        </span>
+      </span>
+    );
   }
 }
